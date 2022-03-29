0b30f75cd196a36418086c42e2a3e5b662ebc067
Ritveek123 <98337785+Ritveek123@users.noreply.github.com>
2022-03-29 14:17:39 +0530

    Create Disk.asm

---
+++ src/Disk.asm
@@ -0,0 +1,21 @@
+PROGRAM_SPACE equ 0x7e00
+ReadDisk:
+  mov ah, 0x02
+  mov al, 1
+  mov ch, 0x00
+  mov cl, 0x02
+  mov dh, 0x00
+  mov dl, [BOOT_DISK]
+  mov bx, PROGRAM_SPACE
+  int 0x13
+  jc DiskErr
+  ret
+BOOT_DISK:
+  db 0
+DiskErrStr:
+  db 0xA, 0xD, "Oops! something went wrong :(", 0
+DiskErr:
+  mov ah, 0x0e
+  mov bx, DiskErrStr
+  call print
+  jmp $
