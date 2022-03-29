8b3c40aab789391d956436cfc262a2bb8a64456f
Ritveek123 <98337785+Ritveek123@users.noreply.github.com>
2022-03-29 14:39:11 +0530

    Update keyboard.asm

--- src/keyboard.asm
+++ src/keyboard.asm
@@ -1,7 +1,6 @@
-  key:
-    mov ah, 0x00
-    int 0x16
-    mov ah, 0x0e
-    int 0x10
-    jmp key
-  times 1024-($-$$) db 0
+key:
+  mov ah, 0x00
+  int 0x16
+  mov ah, 0x0e
+  int 0x10
+  jmp key
