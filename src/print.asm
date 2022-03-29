4c271411d793719457030a969e428f7ffa5854ba
Ritveek123 <98337785+Ritveek123@users.noreply.github.com>
2022-03-29 14:40:17 +0530

    Update print.asm

--- src/print.asm
+++ src/print.asm
@@ -1,13 +1,13 @@
-  mov ah, 0x0e
-  mov bx, wel
-  print:
-    mov al, [bx]
-    cmp al, 0
-    je end
-    int 0x10
-    inc bx
-    jmp print
-  wel:
-    db 0xA, 0xD, "welcome to termux-os", 0xA, 0xD, "you can type any thing you like", 0xA, 0xD, 0
-  end:
-    jmp key
+mov ah, 0x0e
+mov bx, wel
+print:
+  mov al, [bx]
+  cmp al, 0
+  je end
+  int 0x10
+  inc bx
+  jmp print
+wel:
+  db 0xA, 0xD, "welcome to termux-os", 0xA, 0xD, "you can type any thing you like", 0xA, 0xD, 0
+end:
+  jmp key
